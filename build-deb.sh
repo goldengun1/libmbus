890325153047385512325bbd9b7a6d676b95fe8e
Mihailo Simic <simihailo@gmail.com>
2025-04-07 13:48:33 +0200

    Cross-compilation info added to build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -22,4 +22,9 @@ if [ ! -f Makefile ]; then
 fi
 
 debuild -i -us -uc -b 
-#sudo pbuilder build $(NAME)_$(VERSION)-1.dsc
+
+#This is for building a clean 32bit environment because the project is not clearly cross-compilable
+#IMPORTANT: You will need to install additional packages (pbuilder debootstrap)
+#           Then You will need to create a chrooted 32bit environment to buld the entire project in with:
+#           sudo pbuilder create --architecture i386 --distribution focal --debootstrapopts --arch=i386
+#           sudo pbuilder build $(NAME)_$(VERSION)-1.dsc
